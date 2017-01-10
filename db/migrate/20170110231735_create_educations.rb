fabb0c17e318ad2dddef1505e860b5382f82e170
Joel Matos <matosj192@gmail.com>
2017-01-10 18:35:09 -0500

    add models to api

--- db/migrate/20170110231735_create_educations.rb
+++ db/migrate/20170110231735_create_educations.rb
@@ -1,10 +1,10 @@
-class CreateExperiences < ActiveRecord::Migration[5.0]
+class CreateEducations < ActiveRecord::Migration[5.0]
   def change
-    create_table :experiences do |t|
+    create_table :educations do |t|
       t.string :start_date
       t.string :end_date
-      t.string :job_title
-      t.string :company_name
+      t.string :degree
+      t.string :university_name
       t.text :details
       t.integer :student_id
 
