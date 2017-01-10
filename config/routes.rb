333b8bb25755bdb3ac1ce5f8bd5515979fe02681
Dan <dannguyen90@gmail.com>
2017-01-09 21:18:31 -0500

    create new app with controller and routes

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get '/resumes' => 'resumes#index'
+      get '/resumes/:id' => 'resumes#show'
+      post '/resumes' => 'resumes#create'
+      patch '/resumes/:id' => 'resumes#update'
+      delete '/resumes/:id' => 'resumes#destroy'
+    end
+  end
+end
