8d2ff2b01a5d26dc2028f18aefbb3e054c98a096
Dan <dannguyen90@gmail.com>
2017-01-10 20:56:31 -0500

    add show create update destroy pages to Students controller. Also added Matt's resume to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ******* Jigmey *********
 
 Student.create(
   first_name: "Jigmey",
@@ -41,7 +35,7 @@ Experience.create(
   start_date: "2016",
   end_date: "2017",
   job_title: "PC/MAC Support Technician",
-  company_name: "New York City College of Technology",
+  company_name: "CPXi",
   details: "Assisting over 200 users with both hardware and software requests.
 Working back end on Casper imaging, such as creating scripts to make it run smoother.
 Image and deploy computers using Casper(JAMF).
@@ -64,4 +58,72 @@ Skill.create(
 Skill.create(
   skill: "Rails",
   student_id: Student.find_by(first_name: "Jigmey").id
+)
+
+
+# ******* Matt *********
+
+Student.create(
+  first_name: "Matthew",
+  last_name: "Califano",
+  email: "Matthewbcalifano@gmail.com",
+  linkedin_url: "",
+  twitter_handle: "",
+  personal_website_url: "",
+  online_resume_url: "",
+  github_url: "github.com/MattCali",
+  photo: "",
+  phone_number: "732-977-3935",
+  bio: ""
+)
+
+Capstone.create(
+  name: "Perform2nite",
+  description: "",
+  url: "",
+  screenshot: "",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Education.create(
+  start_date: "2012",
+  end_date: "2016",
+  degree: "",
+  university_name: "Brookdale Community College",
+  details: "",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Experience.create(
+  start_date: "2016",
+  end_date: "",
+  job_title: "Actualize  & Apprenticeship",
+  company_name: "",
+  details: "15-week full stack web development training
+Built a fully functional web app from scratch
+Completed daily coding exercises
+Worked in teams and learned to pair program
+Provided maintenance and service for non-profit organization web apps
+",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Skill.create(
+  skill: "Experienced with many mainstream languages and frameworks",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Skill.create(
+  skill: "Strong interpersonal skills resulting in exceptional rapport with people",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Skill.create(
+  skill: "Results-oriented, high-energy, hands-on professional",
+  student_id: Student.find_by(first_name: "Matthew").id
+)
+
+Skill.create(
+  skill: "Proficient in improving customer retention levels within highly competitive markets",
+  student_id: Student.find_by(first_name: "Matthew").id
 )
