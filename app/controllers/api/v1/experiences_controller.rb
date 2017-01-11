4e6f60f07fa88cbad38bf712d2bd62df2c35e2c2
Joel Matos <matosj192@gmail.com>
2017-01-10 19:48:40 -0500

    add create update and destroy to experiences controller

--- app/controllers/api/v1/experiences_controller.rb
+++ app/controllers/api/v1/experiences_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::ExperiencesController < ApplicationController
+  def create
+    @experience = Experience.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+      student_id: params[:student_id,
+      )
+     @experience.save
+     render 'show.json.jbuilder'
+  end
+
+  def update
+    @experience =Experience.find(params[:student_id])
+    @experience.update(
+      start_date: params[:start_date] || @experience.start_date,
+      end_date: params[:end_date] || @experience.end_date,
+      job_title: params[:job_title] || @experience.job_title,
+      company_name: params[:company_name] || @experience.company_name
+      details: params[:details] || @experience.details
+      student_id: params[:student_id] || @experience.student_id
+      )
+  end
+
+  def destroy
+    @experience = Experience.find(params[:student_id])
+    #kill it 
+    @experience.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'v1/Experiences'
-  end
-
-  namespace :api do
     namespace :v1 do
       get '/resumes' => 'resumes#index'
       get '/resumes/:id' => 'resumes#show'
       post '/resumes' => 'resumes#create'
       patch '/resumes/:id' => 'resumes#update'
       delete '/resumes/:id' => 'resumes#destroy'
+      # Experiences
+      post '/experiences' => 'experiences#create'
+      patch '/experiences/:id' => 'experiences#update'
+      delete '/experiences/:id' => 'experiences#destroy'
     end
   end
 end
